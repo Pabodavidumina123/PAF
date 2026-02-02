d4272d32090365802652254af6acbcc3cac4d20d
Pabodavidumina123 <Pabodhavidumina@gmail.com>
2026-02-02 09:57:23 +0530

    Added 1 file

---
+++ practical1.js
@@ -0,0 +1,14 @@
+
+
+function greet(name) {
+    return `Hello, ${name}! Welcome to JavaScript 🚀`;
+}
+
+const user = "Pabodha";
+console.log(greet(user));
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log("Sum of 5 + 3 =", add(5, 3));
